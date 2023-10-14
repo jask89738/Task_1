29b5c8ebce20a249106fd25ab34e6cb3885eaee4
Sass_with_jazz <128492389+jask89738@users.noreply.github.com>
2023-10-14 21:14:23 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,41 @@
-import React, { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [dateOfBirth, setDateOfBirth] = useState('');
-  const [age, setAge] = useState(null);
+  const [count, setCount] = useState(0);
 
-  const calculateAge = () => {
-    if (!dateOfBirth) {
-      alert('Please enter your date of birth.');
-      return;
-    }
-
-    const birthDate = new Date(dateOfBirth);
-    const currentDate = new Date();
-    const ageInMilliseconds = currentDate - birthDate;
-    const ageInYears = Math.floor(ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25));
-    setAge(ageInYears);
-  }
+  const handleInputChange = function () {
+    const textarea = document.getElementById('ok');
+    const words = textarea.value.split(' ');
+   
+    const nonEmptyWords = words.filter(word => word.trim() !== '');
+    setCount(nonEmptyWords.length);
+  };
 
   return (
-    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-blue-700">
-      <div className="bg-white rounded-lg p-8 shadow-md text-center w-96">
-        <h1 className="text-3xl font-extrabold text-blue-700 mb-4">Age Calculator</h1>
-        <p className="text-lg text-gray-600 mb-4">Enter your date of birth</p>
-        <div className="mb-4">
-          <input
-            type="date"
-            name="data"
-            id="data"
-            className="w-full border rounded-lg py-2 px-3"
-            value={dateOfBirth}
-            onChange={(e) => setDateOfBirth(e.target.value)}
-          />
-        </div>
-        <button
-          type="button"
-          onClick={calculateAge}
-          className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-lg px-4 py-2 hover:bg-blue-700 focus:ring focus:ring-blue-400 focus:outline-none"
-        >
-          Calculate Age
-        </button>
-        {age !== null && (
-          <div className="text-xl text-blue-700 mt-4">
-            You are {age} years old.
+    <>
+      <main className="grid h-screen place-items-center">
+        <div className="shadow-2xl p-9">
+          <div className="text-3xl font-serif flex flex-wrap text-center mt-5">Responsive Paragraph Word Counter</div>
+          <div className="relative mb-3 border rounded-lg mt-8" data-te-input-wrapper-init>
+            <textarea
+              className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+              id="ok"
+              rows="6"
+              onChange={handleInputChange} 
+              placeholder="Your message"
+            ></textarea>
+            <label
+              htmlFor="ok"
+              className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+            >
+              Example textarea
+            </label>
           </div>
-        )}
-      </div>
-    </div>
-  )
+          <div>Word count: {count}</div>
+        </div>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
